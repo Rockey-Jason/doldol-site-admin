4151bb1c4973b6f5830c1071de8ecb10784eb417
Rockey-Jason <jason.yeonwoo.im@gmail.com>
2026-09-18 23:58:29 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 // service_role/secret key는 절대로 넣지 마세요.
 window.DORI_ADMIN_CONFIG = {
   SUPABASE_URL: "https://scttowfhygcpdirrekqm.supabase.co",
-  SUPABASE_ANON_KEY: "",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjdHRvd2ZoeWdjcGRpcnJla3FtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxOTg0MjYsImV4cCI6MjA5NTc3NDQyNn0.XwdQhJ4Ku_C61yXz0k65AztMF9Rfe7Qzn3Av7iWRBqY",
   ADMIN_LOGIN_LOOKUP_URL:
     "https://scttowfhygcpdirrekqm.supabase.co/functions/v1/admin-login-email",
   VERIFY_EMAIL_FUNCTION_URL:
