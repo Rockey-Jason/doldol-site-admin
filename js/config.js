013273b5ad6d0d63013d734b23ce50a6bfe50139
Rockey-Jason <jason.yeonwoo.im@gmail.com>
2026-09-18 23:56:18 +0900

    Delete js directory

--- js/config.js
+++
@@ -1,7 +0,0 @@
-// 복사 후 파일명을 config.js로 바꾸고 값을 입력하세요.
-// 실제 배포에서는 anon key만 사용하세요. service_role key를 절대 넣지 마세요.
-window.DORI_ADMIN_CONFIG = {
-  SUPABASE_URL: "https://scttowfhygcpdirrekqm.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjdHRvd2ZoeWdjcGRpcnJla3FtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAxOTg0MjYsImV4cCI6MjA5NTc3NDQyNn0.XwdQhJ4Ku_C61yXz0k65AztMF9Rfe7Qzn3Av7iWRBqY",
-  VERIFY_EMAIL_FUNCTION_URL: "https://scttowfhygcpdirrekqm.supabase.co/functions/v1/admin-request-verification"
-};
